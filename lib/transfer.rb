e840cc63d543044154692988844317350796c5ff
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-11 18:52:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :receiver, :amount, :status
+  
+def initialize (sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = "pending"
+  @counter = 0
+end
+
+def valid?
+  if @sender.valid? == true && @receiver.valid? == true 
+    true 
+  else
+    false 
+  end
+end
+
+ def execute_transaction
+   while @counter < 1 
+   
+   if @sender.balance > @amount && @sender.valid? == true 
+     @sender.balance -= @amount
+     @receiver.balance += @amount
+     @status = "complete"
+   else
+     @status = "rejected"
+     return "Transaction rejected. Please check your account balance."
+   end
+   @counter += 1
+  end
+end
+
+
+  
+
 end
